dc6453882f7deff14be8c0caa7e4bdd08e69ce45
funaika <funaika@ssfs.org>
2023-10-23 19:37:34 -0400

    :)

--- triangles.py
+++ triangles.py
@@ -1,12 +1,12 @@
-def is_triangle(side1, side2, side3):
-    pass
 
+side1 = input("Input longest side of triangle")
+side2 = input("Input second side of triangle")
+side3 = input("Input third side of triangle")
 
+print(side1, side2, side3)
 
-def main():
-    pass
+if side2 + side3 <= side1:
+    print("This cannot make a triangle")
+else: print("This can make a triangle")
 
-
-if __name__ == '__main__':
-    main()
-
+#i have spent 30 minutes on this and need to work on other homework now
