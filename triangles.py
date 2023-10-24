aa49b5abd793b2a714413eae0ebf638429b31f4b
funaika <funaika@ssfs.org>
2023-10-24 19:36:05 -0400

    :)

--- triangles.py
+++ triangles.py
@@ -1,12 +1,12 @@
 
-side1 = input("Input longest side of triangle")
-side2 = input("Input second side of triangle")
-side3 = input("Input third side of triangle")
+side1 = float(input("Input longest side of triangle"))
+side2 = float(input("Input second side of triangle"))
+side3 = float(input("Input third side of triangle"))
 
 print(side1, side2, side3)
 
-if side2 + side3 <= side1:
+if (side1>=side2+side3) or (side2>=side1+side3) or (side3>=side2+side1):
     print("This cannot make a triangle")
-else: print("This can make a triangle")
+else:
+    print("This can make a triangle")
 
-#i have spent 30 minutes on this and need to work on other homework now
