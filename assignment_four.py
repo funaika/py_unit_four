765a6f431f440170bed00e447219f997aa417a20
funaika <funaika@ssfs.org>
2023-11-04 11:44:05 -0400

    :)

--- assignment_four.py
+++ assignment_four.py
@@ -6,45 +6,58 @@ import random
 
 def getCard():
     '''
-    this function gets the computers cards
-    :return: computer total
+    this function generates a random card
+    :return: card random number 1 through 10
     '''
-    cardOne = random.randint(1,10)
-    cardTwo = random.randint(1,10)
-    computerTotal = cardOne + cardTwo
+    card = random.randint(1,10)
+    return card
 
 def userCards():
     '''
     function that gets user cards and figures out who wins
     :return: user total, who the winner is
     '''
-    userCardOne = random.randint(1,10)
-    userCardTwo = random.randint(1,10)
-    userCardThree = random.randint(1, 10)
-    userTotalTwo = userCardThree + userCardTwo + userCardOne
+    userCardOne = getCard()
+    userCardTwo = getCard()
+    userCardThree = getCard()
     print("Your cards are", userCardOne, userCardTwo)
     userTotalOne = userCardTwo + userCardOne
     print("Your total is",userTotalOne)
     anotherCard = float(input("Would you like to draw another card? 1 for y or 2 for n."))
 
+
     if anotherCard == 1:
         print("Your new card it", userCardThree)
-        print("Your new total is ",userCardOne + userCardTwo +userCardThree)
+        userTotalTwo = userCardOne + userCardTwo + userCardThree
+        print("Your new total is ", userTotalTwo)
 
-    dealerCardOne = random.randint(1,10)
-    dealerCardTwo = random.randint(1,10)
+    else:
+        print("Okay, your total is", userTotalOne)
+
+
+    dealerCardOne = getCard()
+    dealerCardTwo = getCard()
     dealerTotal = dealerCardTwo + dealerCardOne
     print("The dealers has a",dealerCardOne, "and a",dealerCardTwo)
     print("The dealers total is",dealerCardTwo + dealerCardOne)
 
-    if userTotalOne > 21 or userTotalTwo > 21:
+    if userTotalOne > 21:
+        print("You went over 21, you lose.")
+    elif anotherCard == 1 and userTotalTwo > 21:
         print("You went over 21, you lose.")
     elif anotherCard == 1 and userTotalTwo > dealerTotal:
         print("User wins!")
-    elif dealerTotal > userTotalTwo or dealerTotal > userTotalOne:
+    elif userTotalOne > dealerTotal:
+        print("User wins!")
+    elif anotherCard == 1 and dealerTotal > userTotalTwo:
         print("Dealer wins.")
-    elif dealerTotal == userTotalOne or dealerTotal == userTotalTwo:
+    elif dealerTotal > userTotalOne:
+        print("Dealer wins.")
+    elif dealerTotal == userTotalOne:
         print("Tie, dealer wins.")
+    elif anotherCard == 1 and dealerTotal == userTotalTwo:
+        print("Tie, dealer wins.")
+
 
 
 def main():
